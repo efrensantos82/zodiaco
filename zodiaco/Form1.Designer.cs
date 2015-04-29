3d2e5b280163281619abd79dc4c05cc4328a794f
efrensantos <efrensantos82@hotmail.com>
2015-04-29 15:06:16 -0500

    tu signo zodiacal

---
+++ zodiaco/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace zodiaco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbmes = new System.Windows.Forms.ComboBox();
+            this.btnsigno = new System.Windows.Forms.Button();
+            this.txtdia = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Californian FB", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DIA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Californian FB", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "MES";
+            // 
+            // cmbmes
+            // 
+            this.cmbmes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbmes.Font = new System.Drawing.Font("Century", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbmes.FormattingEnabled = true;
+            this.cmbmes.Items.AddRange(new object[] {
+            "Enero",
+            "Febrero",
+            "Marzo",
+            "Abril",
+            "Mayo",
+            "Junio",
+            "Julio",
+            "Agosto",
+            "Septiembre",
+            "Octubre",
+            "Noviembre",
+            "Diciembre"});
+            this.cmbmes.Location = new System.Drawing.Point(144, 111);
+            this.cmbmes.Name = "cmbmes";
+            this.cmbmes.Size = new System.Drawing.Size(110, 31);
+            this.cmbmes.TabIndex = 3;
+            // 
+            // btnsigno
+            // 
+            this.btnsigno.Font = new System.Drawing.Font("Century", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsigno.Location = new System.Drawing.Point(122, 201);
+            this.btnsigno.Name = "btnsigno";
+            this.btnsigno.Size = new System.Drawing.Size(95, 35);
+            this.btnsigno.TabIndex = 4;
+            this.btnsigno.Text = "Signo";
+            this.btnsigno.UseVisualStyleBackColor = true;
+            this.btnsigno.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtdia
+            // 
+            this.txtdia.Font = new System.Drawing.Font("Century", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdia.Location = new System.Drawing.Point(144, 53);
+            this.txtdia.Name = "txtdia";
+            this.txtdia.Size = new System.Drawing.Size(110, 30);
+            this.txtdia.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 274);
+            this.Controls.Add(this.txtdia);
+            this.Controls.Add(this.btnsigno);
+            this.Controls.Add(this.cmbmes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbmes;
+        private System.Windows.Forms.Button btnsigno;
+        private System.Windows.Forms.TextBox txtdia;
+    }
+}
+
